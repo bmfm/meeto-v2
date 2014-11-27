3a8f0cf1103231028e838bc7c779d23e64c6bdf3
Bruno Martins <bruno.martins@mindera.com>
2014-11-27 19:30:43 +0000

    started working on meetings

---
+++ src/pt/uc/dei/actions/MeetingAction.java
@@ -0,0 +1,7 @@
+package pt.uc.dei.actions;
+
+/**
+ * Created by brunomartins on 27/11/14.
+ */
+public class MeetingAction {
+}
---
+++ src/pt/uc/dei/models/MeetingBean.java
@@ -0,0 +1,7 @@
+package pt.uc.dei.models;
+
+/**
+ * Created by brunomartins on 27/11/14.
+ */
+public class MeetingBean {
+}
