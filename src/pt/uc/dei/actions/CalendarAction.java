6c146bdde1f52d7d1863ccc2af795c08902e6b32
Vasco Oliveira <vascozorrinho@gmail.com>
2014-12-08 01:26:10 +0000

    - Função finish login

--- src/pt/uc/dei/actions/CalendarAction.java
+++ src/pt/uc/dei/actions/CalendarAction.java
@@ -3,7 +3,7 @@ package pt.uc.dei.actions;
 import com.opensymphony.xwork2.ActionSupport;
 import org.scribe.builder.ServiceBuilder;
 import org.scribe.builder.api.GoogleApi;
-import org.scribe.model.Token;
+import org.scribe.model.*;
 import org.scribe.oauth.OAuthService;
 
 /**
@@ -13,12 +13,21 @@ public class CalendarAction extends ActionSupport {
 
     private static final String NETWORK_NAME = "Google";
     private static final String AUTHORIZE_URL = "https://www.google.com/accounts/OAuthAuthorizeToken?oauth_token=";
-    private static final String PROTECTED_RESOURCE_URL = "https://docs.google.com/feeds/default/private/full/";
+    //private static final String PROTECTED_RESOURCE_URL = "https://docs.google.com/feeds/default/private/full/";
+    private static final String PROTECTED_RESOURCE_URL_GET_PROFILE = "https://www.googleapis.com/userinfo/v2/me";
     private static final String SCOPE = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar";
     private static final Token EMPTY_TOKEN = null;
     private static final String CALLBACK_URL = "http://localhost:8080/meeto/finishGoogleLogin.action";
     private String authorizationUrl;
     private String code;
+
+    private OAuthService service = new ServiceBuilder()
+            .provider(GoogleApi.class)
+            .apiKey("159167502512-9c66e9am5lj9lrq33v3dant27fjt5k4d.apps.googleusercontent.com")
+            .apiSecret("jtsOyhazWTJUnQjf2FZklf1Q")
+            .callback(CALLBACK_URL)
+            .scope(SCOPE)
+            .build();
 
     public String getCode() {
         return code;
@@ -30,14 +39,6 @@ public class CalendarAction extends ActionSupport {
 
     public String goToLogin() {
         try {
-            OAuthService service = new ServiceBuilder()
-                    .provider(GoogleApi.class)
-                    .apiKey("159167502512-9c66e9am5lj9lrq33v3dant27fjt5k4d.apps.googleusercontent.com")
-                    .apiSecret("jtsOyhazWTJUnQjf2FZklf1Q")
-                    .callback(CALLBACK_URL)
-                    .scope(SCOPE)
-                    .build();
-
 
             System.out.println("=== " + NETWORK_NAME + "'s OAuth Workflow ===");
             System.out.println();
@@ -64,10 +65,17 @@ public class CalendarAction extends ActionSupport {
     }
 
     public String finishGoogleLogin() throws Exception {
-
-
+        Verifier verifier = null;
+        // Verifier verifier = new Verifier(access_token);
+        Token accessToken = service.getAccessToken(EMPTY_TOKEN, verifier);
         System.out.println(code);
 
+        OAuthRequest request = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL_GET_PROFILE);
+        service.signRequest(accessToken, request);
+        Response response = request.send();
+
+
+        //TODO obter registo do user que contem o email da google e marcar como online
 
         return "success";
     }
